d3ba3689f77b4fff0385b95520d7e1fa0c30c209
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2017-12-18 10:19:26 +0530

    :rotating_light: Fix linting with prettier: configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,43 @@
 const path = require("path");
 
 module.exports = {
-	context: path.resolve(__dirname, "./app"),
-	entry: "./index.js",
-	output: {
-		path: path.resolve(__dirname, "./dist"),
-		filename: "bundle.js",
-		publicPath: "/dist/"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/, 
-				exclude: /node_modules/,
-				use: ["babel-loader"]
-			},
-			{
-				test: /node_modules\/JSONStream\/index\.js$/,
-				use: ["shebang-loader", "babel-loader"]
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					{
-						loader: "style-loader"
-					}, {
-						loader: "css-loader"
-					}, {
-						loader: "sass-loader"
-					}
-				]
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			}
-		]
-	},
-	externals: ["ws"]
-}
+  context: path.resolve(__dirname, "./app"),
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /node_modules\/JSONStream\/index\.js$/,
+        use: ["shebang-loader", "babel-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  externals: ["ws"]
+};
